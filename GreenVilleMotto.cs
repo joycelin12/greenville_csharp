47556a88e9c1e2d79dadcc4a53772f3c010988cf
Peiyu Williamson <1305934+joycelin12@users.noreply.github.com>
2021-01-10 13:30:41 -0500

    Add files via upload
    To display a motto

---
+++ GreenVilleMotto.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+class GreenvilleMotto
+{
+   static void Main()
+   {
+    // Write your main here.
+    WriteLine("************************************");
+    WriteLine("*  The stars shine in Greenville.  *");
+    WriteLine("************************************");
+   }
+}
